0244dd57613e5b538302c8ddce9797751ee59ecf
gsingh2594 <gsingh2594@gmail.com>
2017-02-10 20:21:49 -0500

    First Push

---
+++ app/src/main/java/com/example/gurpreetsingh/encircleme/LoginActivity.java
@@ -0,0 +1,179 @@
+package com.example.gurpreetsingh.encircleme;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "LoginActivity";
+    private Button btnLogin, btnLinkToSignUp;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+    private EditText loginInputEmail, loginInputPassword;
+    private TextInputLayout loginInputLayoutEmail, loginInputLayoutPassword;
+    private FirebaseAuth Auth;
+    private FirebaseAuth.AuthStateListener AuthListener;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+
+        AuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+        loginInputLayoutEmail = (TextInputLayout) findViewById(R.id.login_input_layout_email);
+        loginInputLayoutPassword = (TextInputLayout) findViewById(R.id.login_input_layout_password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        loginInputEmail = (EditText) findViewById(R.id.login_input_email);
+        loginInputPassword = (EditText) findViewById(R.id.login_input_password);
+
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLinkToSignUp = (Button) findViewById(R.id.btn_link_signup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+
+        btnLinkToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+        String email = loginInputEmail.getText().toString().trim();
+        String password = loginInputPassword.getText().toString().trim();
+
+        if (!checkEmail()) {
+            return;
+        }
+        if (!checkPassword()) {
+            return;
+        }
+        loginInputLayoutEmail.setErrorEnabled(false);
+        loginInputLayoutPassword.setErrorEnabled(false);
+
+        progressBar.setVisibility(View.VISIBLE);
+        //authenticate user
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private boolean checkEmail() {
+        String email = loginInputEmail.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+
+            loginInputLayoutEmail.setErrorEnabled(true);
+            loginInputLayoutEmail.setError(getString(R.string.err_msg_email));
+            loginInputEmail.setError(getString(R.string.err_msg_required));
+            requestFocus(loginInputEmail);
+            return false;
+        }
+        loginInputLayoutEmail.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+
+        String password = loginInputPassword.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+
+            loginInputLayoutPassword.setError(getString(R.string.err_msg_password));
+            loginInputPassword.setError(getString(R.string.err_msg_required));
+            requestFocus(loginInputPassword);
+            return false;
+        }
+        loginInputLayoutPassword.setErrorEnabled(false);
+        return true;
+    }
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private static boolean isPasswordValid(String password) {
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+}
---
+++ app/src/main/java/com/example/gurpreetsingh/encircleme/SignupActivity.java
@@ -0,0 +1,255 @@
+package com.example.gurpreetsingh.encircleme;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.R.attr.id;
+
+
+public class SignupActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
+    private Button btnSignUp, btnLinkToLogIn;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText signupInputEmail, signupInputPassword;
+    private TextInputLayout signupInputLayoutEmail, signupInputLayoutPassword;
+
+    //defining view objects
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignup;
+    private ProgressDialog progressDialog;
+
+
+    //defining firebaseauth object
+    private FirebaseAuth firebaseAuth;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        signupInputLayoutEmail = (TextInputLayout) findViewById(R.id.signup_input_layout_email);
+        signupInputLayoutPassword = (TextInputLayout) findViewById(R.id.signup_input_layout_password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        signupInputEmail = (EditText) findViewById(R.id.signup_input_email);
+        signupInputPassword = (EditText) findViewById(R.id.signup_input_password);
+
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+        btnLinkToLogIn = (Button) findViewById(R.id.btn_link_login);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+
+            }
+        });
+
+        btnLinkToLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+
+        String email = signupInputEmail.getText().toString().trim();
+        String password = signupInputPassword.getText().toString().trim();
+
+        if (!checkEmail()) {
+            return;
+        }
+        if (!checkPassword()) {
+            return;
+        }
+        signupInputLayoutEmail.setErrorEnabled(false);
+        signupInputLayoutPassword.setErrorEnabled(false);
+
+        progressBar.setVisibility(View.VISIBLE);
+        //create user
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+        Toast.makeText(getApplicationContext(), "You are successfully Registered !!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean checkEmail() {
+        String email = signupInputEmail.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+
+            signupInputLayoutEmail.setErrorEnabled(true);
+            signupInputLayoutEmail.setError(getString(R.string.err_msg_email));
+            signupInputEmail.setError(getString(R.string.err_msg_required));
+            requestFocus(signupInputEmail);
+            return false;
+        }
+        signupInputLayoutEmail.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+
+        String password = signupInputPassword.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+
+            signupInputLayoutPassword.setError(getString(R.string.err_msg_password));
+            signupInputPassword.setError(getString(R.string.err_msg_required));
+            requestFocus(signupInputPassword);
+            return false;
+        }
+        signupInputLayoutPassword.setErrorEnabled(false);
+        return true;
+    }
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private static boolean isPasswordValid(String password) {
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Signup Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        mAuth.addAuthStateListener(mAuthListener);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction0());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction0());
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction0() {
+        Thing object = new Thing.Builder()
+                .setName("Signup Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+}
