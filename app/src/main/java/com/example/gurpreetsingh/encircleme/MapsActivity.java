cefc4db2d77ba1f5bd28d405e48f0847aad0b405
gsingh2594 <gsingh2594@gmail.com>
2017-02-15 20:31:40 -0500

    Login and Map working

--- app/src/main/java/com/example/gurpreetsingh/encircleme/MapsActivity.java
+++ app/src/main/java/com/example/gurpreetsingh/encircleme/MapsActivity.java
@@ -70,7 +70,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         //Initialize Google Play Services
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
