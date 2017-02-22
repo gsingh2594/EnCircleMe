f6e8893e8245139a24c2a156f4e147baca80d0c1
braydens77 <braydens77@gmail.com>
2017-02-22 17:38:23 -0500

    Fixed UserProfileActivity and layout to include the bottom menubar.

--- app/src/main/java/com/example/gurpreetsingh/encircleme/UserProfileActivity.java
+++ app/src/main/java/com/example/gurpreetsingh/encircleme/UserProfileActivity.java
@@ -1,11 +1,13 @@
 package com.example.gurpreetsingh.encircleme;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +31,13 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
     private ImageView editIcon;
 
     private AlertDialog editBioDialog;
+
+    // Views for navigation
+    ImageButton btnAlerts;
+    ImageButton btnMaps;
+    ImageButton btnProfile;
+    ImageButton friends;
+    ImageButton btnSetting;
 
 
     @Override
@@ -62,6 +71,12 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
             }
         });
 
+        //access different activity in ImageButton
+        Alerts();
+        Maps();
+        Friends();
+        Profile();
+        Settings();
     }
 
     public void showEditDialog(){
@@ -75,6 +90,8 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
             public void onClick(DialogInterface dialog, int ID){
                 // User clicked save button
                 saveUserBio();
+                Intent refreshProfile = new Intent(UserProfileActivity.this, UserProfileActivity.class);
+                startActivity(refreshProfile);
             }
         });
         editBioDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -94,6 +111,61 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
         // dbUserRef.setValue(user);    --> Unnecessary because it rewrites the whole User object to the DB
         // use this line instead to update only the "bio" value
         dbUserRef.child("bio").setValue(bio);
+    }
+
+
+
+    // Methods for navigation
+    public void Profile() {
+        btnProfile = (ImageButton) findViewById(R.id.btnProfile);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent time = new Intent(UserProfileActivity.this, UserProfileActivity.class);
+                startActivity(time);
+            }
+        });
+    }
+
+    public void Alerts(){
+        btnAlerts = (ImageButton) findViewById(R.id.btnAlerts);
+        btnAlerts.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent alerts = new Intent(UserProfileActivity.this, UserActivity.class);
+                startActivity(alerts);
+            }
+        });
+    }
+    public void Maps(){
+        btnMaps = (ImageButton) findViewById(R.id.btnMaps);
+        btnMaps.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent alerts = new Intent(UserProfileActivity.this, MapsActivity.class);
+                startActivity(alerts);
+            }
+        });
+    }
+    public void Friends(){
+        friends = (ImageButton) findViewById(R.id.friends);
+        friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent offMaps = new Intent(UserProfileActivity.this, UserActivity.class);
+                startActivity(offMaps);
+            }
+        });
+    }
+    public void Settings(){
+        btnSetting = (ImageButton) findViewById(R.id.setting);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent setting = new Intent(UserProfileActivity.this, UserActivity.class);
+                startActivity(setting);
+            }
+        });
     }
 
     @Override
