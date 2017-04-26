26749c6b281de903ace29178cb0ae36929355fa1
braydens77 <braydens77@gmail.com>
2017-04-26 02:56:33 -0400

    EventListActivity no longer shows events that have ended. EventListActivity sorts events by start date and start time

--- app/src/main/java/com/example/gurpreetsingh/encircleme/EventListActivity.java
+++ app/src/main/java/com/example/gurpreetsingh/encircleme/EventListActivity.java
@@ -133,13 +133,15 @@ public class EventListActivity extends AppCompatActivity {
                         Log.d("event info", event.toString());
                         // Check if event has ended
                         if(!eventHasEnded(event)) {
-                            // Store event info in a hashmap
+                            // Event has not ended --> store event info in a hashmap
                             HashMap<String, String> eventInfo = new HashMap<String, String>();
                             eventInfo.put("eventKey", child.getKey());
                             eventInfo.put("name", event.getName());
                             eventInfo.put("description", event.getAbout());
                             eventInfo.put("startDate", event.getDate());
                             eventInfo.put("startTime", event.getStartTime());
+                            eventInfo.put("endDate", event.getEndDate());
+                            eventInfo.put("endTime", event.getEndTime());
                             // Add event info hashmap to the events arraylist
                             eventsList.add(eventInfo);
                         }
@@ -149,9 +151,10 @@ public class EventListActivity extends AppCompatActivity {
                     Comparator eventDatesAndTimesComparator = new Comparator<HashMap<String, String>>() {
                         @Override
                         public int compare(HashMap<String, String> event1, HashMap<String, String> event2) {
-                            String[] startMDY1 = event1.get("startDate").split("/");
-                            String[] startMDY2 = event2.get("startDate").split("/");
-                            int year1 = Integer.parseInt(startMDY1[2]);
+                            /*String[] startMDY1 = event1.get("startDate").split("/");
+                            String[] startMDY2 = event2.get("startDate").split("/"); */
+
+                            /*int year1 = Integer.parseInt(startMDY1[2]);
                             int year2 = Integer.parseInt(startMDY2[2]);
                             if(year1 < year2)
                                 return -1; // first event date is before the second event
@@ -173,11 +176,67 @@ public class EventListActivity extends AppCompatActivity {
                                         return 1;
                                     else{ // days are equal --> check times
                                         return 0;
-
                                     }
                                 }
-                            }
+                            } */
+                            int event1Month, event1Day, event1Year, event1Hour, event1Min;
+                            int event2Month, event2Day, event2Year, event2Hour, event2Min;
 
+                            // Get event1 start month, day, year
+                            String[] mdy1 = event1.get("startDate").split("/");
+                            event1Month = Integer.parseInt(mdy1[0]);
+                            event1Month-=1; // decrement month by 1 because range of months is 0-11
+                            event1Day = Integer.parseInt(mdy1[1]);
+                            event1Year = Integer.parseInt(mdy1[2]);
+
+                            // Get event2 start month, day, year
+                            String[] mdy2 = event2.get("startDate").split("/");
+                            event2Month = Integer.parseInt(mdy2[0]);
+                            event2Month-=1; // decrement month by 1 because range of months is 0-11
+                            event2Day = Integer.parseInt(mdy2[1]);
+                            event2Year = Integer.parseInt(mdy2[2]);
+
+                            // Get event1 start hour and min
+                            String hourMin1[] = event1.get("startTime").split("[: ]");
+                            if(hourMin1[2].equals("pm"))
+                                if(Integer.parseInt(hourMin1[0]) != 12)
+                                    event1Hour = Integer.parseInt(hourMin1[0])+ 12; // If pm and hour!=12, add 12 to the hour to convert to 24 hour format
+                                else
+                                    event1Hour = Integer.parseInt(hourMin1[0]);
+                            else
+                                event1Hour = Integer.parseInt(hourMin1[0]); // Hour is in am
+                            event1Min = Integer.parseInt(hourMin1[1]);
+
+                            // Get event2 start hour and min
+                            String[] hourMin2 = event2.get("startTime").split("[: ]");
+                            if(hourMin2[2].equals("pm"))
+                                if(Integer.parseInt(hourMin2[0]) != 12)
+                                    event2Hour = Integer.parseInt(hourMin2[0])+ 12; // If pm and hour!=12, add 12 to the hour to convert to 24 hour format
+                                else
+                                    event2Hour = Integer.parseInt(hourMin2[0]);
+                            else
+                                event2Hour = Integer.parseInt(hourMin2[0]); // Hour is in am
+                            event2Min = Integer.parseInt(hourMin2[1]);
+
+
+                            // Calendar for event1
+                            Calendar event1Calendar = Calendar.getInstance();
+                            event1Calendar.clear(); // Clear the fields in the calendar so the compareTo method works properly
+                            event1Calendar.set(event1Year, event1Month, event1Day, event1Hour, event1Min, 0);
+                            //event1Calendar.getTimeInMillis(); // get to recalcculate time in millis
+                            Log.d("event1Calendar", "after setting calendar: " + Long.toString(event1Calendar.getTimeInMillis()));
+
+                            // Calendar for event1
+                            Calendar event2Calendar = Calendar.getInstance();
+                            event2Calendar.clear(); // Clear the fields in the calendar so the compareTo method works properly
+                            event2Calendar.set(event2Year, event2Month, event2Day, event2Hour, event2Min, 0);
+                            //event1Calendar.getTimeInMillis(); // get to recalcculate time in millis
+                            Log.d("event2Calendar", "after setting calendar: " + Long.toString(event2Calendar.getTimeInMillis()));
+
+                            // Compare the event1 and event2 calendars and and return the result
+                            int result = event1Calendar.compareTo(event2Calendar);
+                            Log.d("comparator result", Integer.toString(result));
+                            return result;
                         }
                     };
 
@@ -213,32 +272,64 @@ public class EventListActivity extends AppCompatActivity {
             });
     }
 
+
     // returns true if event end date is before the current time, or false otherwise
     private boolean eventHasEnded(Event event){
         int endMonth, endDay, endYear;
         if(event.getEndDate() != null) {
+            // End date exists --> use it
             String[] mdy = event.getEndDate().split("/");
-            endMonth = Integer.parseInt(mdy[0]);
+            endMonth = Integer.parseInt(mdy[0]) -1; // decrement month by 1 because range of months is 0-11
             endDay = Integer.parseInt(mdy[1]);
             endYear = Integer.parseInt(mdy[2]);
         }
         else{
+            // No end date given --> use start date
             String[] mdy = event.getDate().split("/");
-            endMonth = Integer.parseInt(mdy[0]);
+            endMonth = Integer.parseInt(mdy[0]) -1; // decrement month by 1 because range of months is 0-11
             endDay = Integer.parseInt(mdy[1]);
             endYear = Integer.parseInt(mdy[2]);
         }
 
         String hourMin[] = event.getEndTime().split("[: ]");
-        int hour = hourMin[2].equals("pm")?
-                // If pm, add 12 to the hour to convert to 24 hour format
-                Integer.parseInt(hourMin[0])+ 12 : Integer.parseInt(hourMin[0]);
-        for(String elem : hourMin)
-            Log.d("hourMin", elem);
+        int endHour;
+        if(hourMin[2].equals("pm"))
+            if(Integer.parseInt(hourMin[0]) != 12)
+                endHour = Integer.parseInt(hourMin[0])+ 12; // If pm and hour!=12, add 12 to the hour to convert to 24 hour format
+            else
+                endHour = Integer.parseInt(hourMin[0]);
+        else
+            endHour = Integer.parseInt(hourMin[0]); // Hour is in am
+
+        int endMin = Integer.parseInt(hourMin[1]);
+
+        /*for(String elem : hourMin)
+            Log.d("hourMin", elem);*/
+        Log.d("endYear", Integer.toString(endYear));
+        Log.d("endMonth", Integer.toString(endMonth));
+        Log.d("endDay", Integer.toString(endDay));
+        Log.d("endHour", Integer.toString(endHour));
+        Log.d("endMin", Integer.toString(endMin));
 
         Calendar endCalendar = Calendar.getInstance();
-        endCalendar.set(endYear, endMonth, endDay);
-        return false;
+        Log.d("endCalendar", Long.toString(endCalendar.getTimeInMillis()));
+        endCalendar.clear(); // Clear the fields in the calendar so the compareTo method works properly
+        endCalendar.set(endYear, endMonth, endDay, endHour, endMin, 0);
+        endCalendar.getTimeInMillis(); // get to recalcculate time in millis
+        Log.d("endCalendar", "after setting calendar: " + Long.toString(endCalendar.getTimeInMillis()));
+
+        // Get current time
+        Calendar currentCalendar = Calendar.getInstance();
+        Log.d("currentCalendar", Long.toString(currentCalendar.getTimeInMillis()));
+        // Compare current time to event end time
+        int result = endCalendar.compareTo(currentCalendar);
+        Log.d("eventHasEnded", "result= " + Integer.toString(result));
+        if(result == -1) // End time is before current time
+            return true; // --> Event has ended
+        if(result == 1)  // End time is after end time
+            return false;// --> Event has not ended
+        else             // End time and current time are equal
+            return true; // --> event has ended
     }
 
 
